abc658e5854a1cc565056a0ec006853ca234735a
juanmarrom <juan.martinez.02912@uols.org>
2018-06-11 12:46:02 -0700

    Ultimos ajustes

--- obtener_codigo_postal.php
+++ obtener_codigo_postal.php
@@ -1,20 +1,11 @@
 <?php 
-// function to geocode address, it will return false if unable to geocode address
-//https://maps.googleapis.com/maps/api/geocode/json?address=toma%20morales%2099%20las%20palmas&key=AIzaSyCEUxuelm-ruuX7STQP7iDdk-KpoRedKCY
-//http://maps.google.com/maps?q=24.197611,120.780512
 
-$address = "tomas morales 99 las palmas";
-// url encode the address
-$address = urlencode($address);
-// google map geocode api url
-$url = "https://maps.googleapis.com/maps/api/geocode/json?address={$address}&key=AIzaSyCEUxuelm-ruuX7STQP7iDdk-KpoRedKCY"; 
-// get the json response
+$direccion_org = "av diagonal 245 Barcelona catalunya";
+$direccion_org = urlencode($direccion_org);
+$url = "https://maps.googleapis.com/maps/api/geocode/json?address={$direccion_org}&key=AIzaSyCEUxuelm-ruuX7STQP7iDdk-KpoRedKCY"; 
 $resp_json = file_get_contents($url);     
-// decode the json
 $resp = json_decode($resp_json, true); 
-// response status will be 'OK', if able to geocode given address 
 if($resp['status'] == 'OK') { 
-	// get the important data
 	echo "OK <br>";	
 	$direccion = $resp['results'][0]['address_components'];
 	$max = sizeof($direccion);
